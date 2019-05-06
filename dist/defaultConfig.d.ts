c3b03173a3dfc3203a915584d07d541562cd4c80
Magnus Tovslid <magnus.tovslid@gmail.com>
2019-05-06 13:33:06 +0200

    Published by publish-to-git 9bb931f9b5392b49791b1f15d7228a64efc389d1 (HEAD -> refs/heads/master, refs/remotes/origin/master) Bump v

---
+++ dist/defaultConfig.d.ts
@@ -0,0 +1,12 @@
+declare const _default: {
+    optionsAuth0: {
+        clientID: string;
+        domain: string;
+        configurationBaseUrl: string;
+    };
+    identityApiUrl: string;
+    loginUrl: () => string;
+    logoutUrl: () => string;
+    callbackUrl: string;
+};
+export default _default;
