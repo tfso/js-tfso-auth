2d447087a464cae0fe4862e4d2f624786ee63889
Magnus Tovslid <magnus.tovslid@gmail.com>
2019-06-20 09:17:39 +0200

    Published by publish-to-git 7ed264a254843711a5ffcb91ead1127ba43a7c3e (HEAD -> refs/heads/master, refs/remotes/origin/master) Bump v

---
+++ dist/Authenticator.d.ts
@@ -0,0 +1,12 @@
+import * as types from './types';
+export declare class Authenticator {
+    _config: types.AuthenticatorConfig;
+    _webAuth: types.WebAuthPromisified;
+    constructor(config: Partial<types.AuthenticatorConfig>);
+    getCurrentlyLoggedInIdentityOrNull(): Promise<any>;
+    ensureLoggedIn(): Promise<any>;
+    redirectToLogin(): void;
+    redirectToLogout(): void;
+    _getIdentityOrNullIfCookieRequired(): Promise<any>;
+    _getIdentityApiTokenOrNulIfAuthRequired(): Promise<import("auth0-js").Auth0DecodedHash | null>;
+}
