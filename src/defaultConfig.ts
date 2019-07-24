2a303b3c44c51001f1faf8e039d858de01c93862
Magnus Tovslid <magnus.tovslid@gmail.com>
2019-07-24 14:55:28 +0200

    Point to new auth website

--- src/defaultConfig.ts
+++ src/defaultConfig.ts
@@ -6,7 +6,7 @@ export default {
     },
     identityApiUrl: 'https://identity.api.24sevenoffice.com',
     authenticateJwtUrl: '/login/data/AuthenticateJwt.aspx',
-    loginUrl: () => `/login/?returnUrl=${encodeURIComponent(window.location.origin + window.location.pathname)}`,
-    logoutUrl: () => `/login/auth0/logout.html`,
-    callbackUrl: `${window.location.origin}/login/auth0/callback.html?isSilent=true`
+    loginUrl: () => `/modules/auth/login/?returnUrl=${encodeURIComponent(window.location.origin + window.location.pathname)}`,
+    logoutUrl: () => `/modules/auth/logout`,
+    callbackUrl: `${window.location.origin}/modules/auth/login-callback?isSilent=true`
 }
