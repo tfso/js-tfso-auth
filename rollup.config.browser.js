581b156991658423b1382de1b410beb84685e041
lostfields <nicolai.gjaerum@gmail.com>
2023-01-30 10:51:39 +0100

    fix: browser output

--- rollup.config.browser.js
+++ rollup.config.browser.js
@@ -1,13 +1,12 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import pkg from './package.json'
 
 export default {
     input: 'src/index.ts',
     output: [
         {
-            file: pkg.globalModule,
+            file: 'dist/TfsoAuth.js',
             format: 'iife',
             exports: 'named',
             name: 'TfsoAuth'
